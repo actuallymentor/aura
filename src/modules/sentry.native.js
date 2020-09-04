033302396fa4e23c134178a42880126abb34374c
Mentor Palokaj <mentor@palokaj.co>
2020-09-04 13:38:18 +0200

    Hasty deployment made me leave in debug parameters

--- src/modules/sentry.native.js
+++ src/modules/sentry.native.js
@@ -4,9 +4,8 @@ import { SENTRY_DSN } from 'react-native-dotenv'
 const SentryInit = f => {
 	if( SENTRY_DSN ) Sentry.init( {
 	  dsn: SENTRY_DSN,
-	  enableInExpoDevelopment: true
+	  enableInExpoDevelopment: false
 	} )
-	console.log('Sentry: ', Object.keys( Sentry ) )
 	return Sentry
 }
 
